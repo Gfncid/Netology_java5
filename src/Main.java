ed720dfbd427a5c0931c2a7ea0f987436339ad81
Aleksandr Zarubin <aljkeo.O@gmail.com>
2020-06-16 14:29:34 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int cost = 4000;
-                int spend = 20;
-                        int miles = cost / spend;
+        int spend = 20;
+        int miles = cost / spend;
         System.out.println(miles);
     }
 }
