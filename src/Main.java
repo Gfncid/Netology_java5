802cad1fbc2ae1e6199b9fa3a9745d88a0399570
Aleksandr Zarubin <aljkeo.O@gmail.com>
2020-06-18 09:02:42 +0300

    add BonusmilesService and modify code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 4000;
-        int spend = 20;
-        int miles = cost / spend;
-        System.out.println(miles);
+        BonusMilesService service = new BonusMilesService();
+
+        int miles1 = service.calculate(10000);
+        System.out.println(miles1);
+
+        int miles2 = service.calculate(500);
+        System.out.println(miles2);
+
+        int miles3 = service.calculate(1000);
+        System.out.println(miles3);
+
+        int miles4 = service.calculate(25000);
+        System.out.println(miles4);
     }
 }
